7db86500f2496d36a689856380c0d53ba3c773e0
Stefan <stefanseifeddine@gmail.com>
2023-04-25 01:25:51 +0100

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+    return (
+        <header>
+            <Link to="/">
+                <h1>Shopping Cart</h1>
+            </Link>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/shop">Shop</Link>
+            </nav>
+        </header>
+    );
+}
