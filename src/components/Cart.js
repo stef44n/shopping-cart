453c13490f67ef44a52fb54cdfca0d8b5fa97571
Stefan <stefanseifeddine@gmail.com>
2023-04-26 00:38:17 +0100

    Add cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+// import pic from "../assets/pineapple.webp";
+// import "../styles/Home.css";
+
+export default function Cart() {
+    return (
+        <div>
+            shop Cart
+            <p>empty cart</p>
+            {/* <img src={pic} alt="card"></img> */}
+        </div>
+    );
+}
