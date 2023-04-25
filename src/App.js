13c572e4bd58b89202bf4d715d9aaae785d624ef
Stefan <stefanseifeddine@gmail.com>
2023-04-25 01:30:20 +0100

    Add a navbar and routes for home and shop pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
