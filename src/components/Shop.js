2004b8f3cb610deec15e31e983718e825b461fd7
Stefan <stefanseifeddine@gmail.com>
2023-04-26 01:11:17 +0100

    Add products from fruit array onto the page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,21 @@
+import fruitsArray from "../assets/fruits";
+import Card from "./Card";
+
 export default function Shop() {
+    const allFruits = fruitsArray.map((fruit) => (
+        <Card
+            key={fruit.id}
+            name={fruit.name}
+            price={fruit.price}
+            imageSource={fruit.image}
+        />
+    ));
+
     return (
         <div>
             Shopping page
             <p>products will be here</p>
+            {allFruits}
         </div>
     );
 }
