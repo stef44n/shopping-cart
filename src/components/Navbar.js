d1b665d2666c7cfad7af05c5259eb78ff97bd557
Stefan <stefanseifeddine@gmail.com>
2023-04-26 01:08:13 +0100

    Add a link to the cart page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ export default function Navbar() {
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/shop">Shop</Link>
+                <Link to="/cart">Cart</Link>
             </nav>
         </header>
     );
