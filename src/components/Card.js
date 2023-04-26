8f948f53cf2709eb80714bd25b06ee53fbcff077
Stefan <stefanseifeddine@gmail.com>
2023-04-26 15:47:30 +0100

    Add quantity state and buttons to adjust it

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,49 @@
 import React from "react";
-// import "../styles/Card.css";
+import "../styles/Card.css";
 
 export default function Card(props) {
     //set card state here
-    // const [clickedState, setClickedState] = React.useState(false);
+    const [quantity, setQuantity] = React.useState(1);
 
     // let resetAll = setClickedState(props.reSet);
     // let resetAll = props.reSet ? setClickedState(true) : false;
     // console.log(`%c PROPS.RESET: ${resetAll}`, "background: lightgrey");
 
-    // function handleCardClick() {
-    //
-    // }
+    function decrementQuantity() {
+        setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+
+    function incrementQuantity() {
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    }
 
     return (
         <div
-            // className="card"
+            className="card"
             // className={`card ${clickedState}`}
             // value={`${clickedState}`}
             onClick={props.handleClick}
         >
+            <h2>{props.name}</h2>
             <img
                 className="card-image"
                 src={props.imageSource}
                 alt="card"
                 // data-value={`${clickedState}`}
-                // onClick={() => handleCardClick(props.reSet)}
-                // onClick={() => handleCardClick(false)}
             />
-            <p>{props.name}</p>
-            <p>{props.price}</p>
+            <p>${props.price}</p>
+            <div className="quantity">
+                <button
+                    onClick={decrementQuantity}
+                    disabled={quantity === 0 ? true : false}
+                >
+                    -
+                </button>
+
+                <p>{quantity}</p>
+                <button onClick={incrementQuantity}>+</button>
+            </div>
+            <button>Add to cart</button>
         </div>
     );
 }
