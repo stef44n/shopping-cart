35f245c37681b8ebffd440926ae44accb033bec1
Stefan <stefanseifeddine@gmail.com>
2023-04-28 19:35:44 +0100

    Get card data and display quantity on page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,8 @@
 import fruitsArray from "../assets/fruits";
 import Card from "./Card";
+import "../styles/Shop.css";
+import React from "react";
+import cartIcon from "../assets/icons/cart.svg";
 
 export default function Shop() {
     const allFruits = fruitsArray.map((fruit) => (
@@ -8,14 +11,25 @@ export default function Shop() {
             name={fruit.name}
             price={fruit.price}
             imageSource={fruit.image}
+            childToParent={childToParent}
         />
     ));
+
+    const [data, setData] = React.useState(0);
+
+    function childToParent(childData) {
+        setData((prevData) => prevData + childData);
+    }
 
     return (
         <div>
             Shopping page
             <p>products will be here</p>
-            {allFruits}
+            <div className="cart-fixed">
+                <img className="cart-icon" src={cartIcon} alt="cart-icon" />(
+                {data})
+            </div>
+            <div className="products">{allFruits}</div>
         </div>
     );
 }
