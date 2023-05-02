012cfb79f6f57f45d95a4c38e3afa5c4ae88a79b
Stefan <stefanseifeddine@gmail.com>
2023-05-02 01:10:09 +0100

    Add page toggle and display items in cart

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,7 +4,7 @@ import Cart from "./Cart";
 import "../styles/Shop.css";
 import React from "react";
 import cartIcon from "../assets/icons/cart.svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Shop() {
     const allFruits = fruitsArray.map((fruit) => (
@@ -18,7 +18,9 @@ export default function Shop() {
     ));
 
     const [cartQty, setCartQty] = React.useState(0);
-    const [cartItems, setCartItems] = React.useState("empty");
+    const [cartItems, setCartItems] = React.useState(["empty"]);
+
+    const [togglePage, setTogglePage] = React.useState(true);
 
     function childToParent(childData) {
         let qty = childData.qty;
@@ -36,18 +38,51 @@ export default function Shop() {
         console.log(cartItems);
     }
 
+    function shopCartToggle() {
+        setTogglePage((prevData) => !prevData);
+    }
+
+    const allCartItems = cartItems.map((item) => (
+        <Cart
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            imageSource={item.image}
+            quantity={item.qty}
+            total={item.total}
+        />
+    ));
+
     return (
         <div>
             Shopping page
             <p>products will be here</p>
-            <Link to="/cart">
-                <div className="cart-fixed">
-                    <img className="cart-icon" src={cartIcon} alt="cart-icon" />
-                    ({cartQty})
+            {/* <Link to="/cart"> */}
+            <div className="cart-fixed">
+                <img className="cart-icon" src={cartIcon} alt="cart-icon" />(
+                {cartQty})
+            </div>
+            {/* </Link> */}
+            <button onClick={() => shopCartToggle()}>
+                Show cart / hide shop
+            </button>
+            {togglePage && <div className="products">{allFruits}</div>}
+            {!togglePage && cartQty === 0 && (
+                <div className="cart">The cart is empty...</div>
+            )}
+            {!togglePage && cartQty !== 0 && (
+                <div className="cart">
+                    The cart is NOT empty... items in cart: {cartQty}
+                    <div className="cart-card">
+                        <h2> </h2>
+                        <h2>Item</h2>
+                        <h2>Price</h2>
+                        <h2>Quantity</h2>
+                        <h2>Total</h2>
+                    </div>
+                    {allCartItems}
                 </div>
-            </Link>
-            <div className="products">{allFruits}</div>
-            <Cart />
+            )}
         </div>
     );
 }
