ee4a802bb5ee915973a090f00914c70cb5844fd8
Stefan <stefanseifeddine@gmail.com>
2023-05-02 01:12:15 +0100

    Add template for cart items display

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,40 @@
+import React from "react";
 // import pic from "../assets/pineapple.webp";
 // import "../styles/Home.css";
+// import Shop from "./Shop";
+import "../styles/Cart.css";
 
-export default function Cart() {
+// Cart Item
+export default function Cart(props) {
+    // const [cart, setCart] = React.useState([]);
+    // const [cartData, setCartData] = React.useState([]);
+
+    // const itemsInCart = ""; //map
+
+    const [cartItemState, setCartItemState] = React.useState({
+        name: props.name,
+        price: props.price,
+        image: props.imageSource,
+        qty: props.quantity,
+        total: props.total,
+    });
+
+    // const cartFunction = props.shopToCart;
+
     return (
-        <div>
-            shop Cart
-            <p>empty cart</p>
-            {/* <img src={pic} alt="card"></img> */}
+        <div className="cart-card">
+            {
+                <img
+                    className="cart-image"
+                    src={props.imageSource}
+                    alt={props.name}
+                />
+            }
+            {<p>{props.name}</p>}
+            {<p>{props.price}</p>}
+            {<p>{props.quantity}</p>}
+            {<p>{props.total}</p>}
+            {/* {cartFunction} */}
         </div>
     );
 }
