6d90e019009f13d3329957012cc64b0ae156ac2a
Stefan <stefanseifeddine@gmail.com>
2023-05-03 20:29:26 +0100

    Add home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,28 @@
+import pic from "../assets/pineapple.webp";
+import pic2 from "../assets/banana.jpg";
+import pic3 from "../assets/blueberry.jpg";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
-        <div>
-            Homepage
-            <p>welcome to ...</p>
+        <div className="home-squares">
+            <h1>
+                [Fruits] <br /> ...on demand
+            </h1>
+            <img src={pic} alt="card"></img>
+            <img src={pic2} alt="card"></img>
+            <img src={pic3} alt="card"></img>
+            <div className="centre-div">
+                <h2>Your 5-a-day starts here!</h2>
+                <Link to="/shop">
+                    <button>Shop now!</button>
+                </Link>
+            </div>
+            <img src={pic} alt="card"></img>
+            <img src={pic2} alt="card"></img>
+            <img src={pic3} alt="card"></img>
+            <h2>'Where math jokes become reality'</h2>
         </div>
     );
 }
