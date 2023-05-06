1403272f08520c6805abff7ec41caa44039dc256
Stefan <stefanseifeddine@gmail.com>
2023-05-06 22:40:53 +0100

    Add code for improving the project (commented out)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,46 @@
 import React from "react";
-// import pic from "../assets/pineapple.webp";
-// import "../styles/Home.css";
-// import Shop from "./Shop";
 import "../styles/Cart.css";
+// import minus from "../assets/icons/minus.svg";
+// import plus from "../assets/icons/plus.svg";
 
 // Cart Item
 export default function Cart(props) {
-    // const [cart, setCart] = React.useState([]);
-    // const [cartData, setCartData] = React.useState([]);
+    // const [itemQuantity, setItemQuantity] = React.useState(props.quantity);
+    // console.log(`state of ${props.name}: ${itemQuantity}`);
 
-    // const itemsInCart = ""; //map
+    // const [cartItemState, setCartItemState] = React.useState({
+    //     id: props.id,
+    //     name: props.name,
+    //     price: props.price,
+    //     image: props.imageSource,
+    //     qty: props.quantity,
+    //     total: props.total,
+    // });
 
-    const [cartItemState, setCartItemState] = React.useState({
-        name: props.name,
-        price: props.price,
-        image: props.imageSource,
-        qty: props.quantity,
-        total: props.total,
-    });
+    // function decrementQuantity() { // REFINING PROJECT NEXT STEP: adjust individual quantities in cart
+    //     setItemQuantity((prevQuantity) => prevQuantity - 1);
+    // }
 
-    // const cartFunction = props.shopToCart;
+    // function incrementQuantity() {
+    //     setItemQuantity((prevQuantity) => prevQuantity + 1);
+    // }
+
+    // React.useEffect(() => {
+    //     //
+    //     setCartItemState((prevState) => {
+    //         return {
+    //             ...prevState,
+    //             qty: itemQuantity,
+    //             total: (
+    //                 Math.round(Number(prevState.price) * itemQuantity * 100) /
+    //                 100
+    //             ).toFixed(2),
+    //         };
+    //     });
+    // }, [itemQuantity]);
 
     return (
-        <div className="cart-card">
+        <div className="cart-card item">
             {
                 <img
                     className="cart-image"
@@ -32,9 +50,37 @@ export default function Cart(props) {
             }
             {<p>{props.name}</p>}
             {<p>{props.price}</p>}
-            {<p>{props.quantity}</p>}
+            {
+                // <div className="quantity"> // REFINING PROJECT NEXT STEP: +/- buttons
+                //     <button
+                //         onClick={() => {
+                //             decrementQuantity();
+                //             props.alterCartItemQuantity(cartItemState);
+                //         }}
+                //         // disabled={quantity === 1 ? true : false}
+                //     >
+                //         <img
+                //             className="quantity-button"
+                //             src={minus}
+                //             alt="subtract item"
+                //         />
+                //     </button>
+                <p>{props.quantity}</p>
+                //     <button // REFINING PROJECT NEXT STEP
+                //         onClick={() => {
+                //             incrementQuantity();
+                //             props.alterCartItemQuantity(cartItemState);
+                //         }}
+                //     >
+                //         <img
+                //             className="quantity-button"
+                //             src={plus}
+                //             alt="add item"
+                //         />
+                //     </button>
+                // </div>
+            }
             {<p>{props.total}</p>}
-            {/* {cartFunction} */}
         </div>
     );
 }
