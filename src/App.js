2b0d92349b0d6da8d2f410cbcc7ad8748cb8e8c9
Stefan <stefanseifeddine@gmail.com>
2023-05-06 23:08:11 +0100

    Delete cart path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Shop from "./components/Shop";
+import "./styles/App.css";
 
 function App() {
     return (
@@ -11,7 +11,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
-                <Route path="/cart" element={<Cart />} />
             </Routes>
         </BrowserRouter>
     );
